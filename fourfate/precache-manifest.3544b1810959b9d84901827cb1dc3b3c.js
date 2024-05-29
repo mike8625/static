4e28af8c342c3e0d148b46e756ae3e12d5bf496b
mike8625 <mike8625@163.com>
2024-05-29 09:40:13 +0800

    fourfate

---
+++ fourfate/precache-manifest.3544b1810959b9d84901827cb1dc3b3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9811e247e3e828ec42a3b72a77f4aaae",
+    "url": "./static/media/wm2.9811e247.png"
+  },
+  {
+    "revision": "ab9cdb87c84ed323de50441622bd0353",
+    "url": "./static/media/wm.ab9cdb87.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "e5299cca7e24a995a3c4",
+    "url": "./static/js/main.e5299cca.chunk.js"
+  },
+  {
+    "revision": "017e474a09aca047d51c",
+    "url": "./static/js/2.017e474a.chunk.js"
+  },
+  {
+    "revision": "e5299cca7e24a995a3c4",
+    "url": "./static/css/main.38a073cd.chunk.css"
+  },
+  {
+    "revision": "017e474a09aca047d51c",
+    "url": "./static/css/2.8a29d12e.chunk.css"
+  },
+  {
+    "revision": "3891d6efbda3c1461c6d21b4bbeb5e41",
+    "url": "./index.html"
+  }
+];
