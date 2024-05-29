c51a3d19d9197617c740394b0288a4f465c0fd62
mike8625 <mike8625@163.com>
2024-05-29 09:39:59 +0800

    audio compare

---
+++ audiocompare/precache-manifest.0db2c348e9ccfc46abdea54ee4e7e58f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffeb849dce8c36e7314cb00fa69cd228",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62313a491bfadfa3ee42",
+    "url": "./static/css/2.b6719d87.chunk.css"
+  },
+  {
+    "revision": "cc1a65014bde5e31d03b",
+    "url": "./static/css/main.3b288ab9.chunk.css"
+  },
+  {
+    "revision": "62313a491bfadfa3ee42",
+    "url": "./static/js/2.99cf2479.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "./static/js/2.99cf2479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1a65014bde5e31d03b",
+    "url": "./static/js/main.d2b1c76a.chunk.js"
+  },
+  {
+    "revision": "ce6fbf35b9a4a9802f6f",
+    "url": "./static/js/runtime-main.d7d210e9.js"
+  },
+  {
+    "revision": "86569926dc03a11ddb7baa895ff9aff1",
+    "url": "./static/media/bg.86569926.jpg"
+  },
+  {
+    "revision": "5609c541cc455db2708b8eb25517120f",
+    "url": "./static/media/iconfont.5609c541.ttf"
+  },
+  {
+    "revision": "f6304b27641bea22db41ecd355e551fc",
+    "url": "./static/media/iconfont.f6304b27.svg"
+  }
+]);
